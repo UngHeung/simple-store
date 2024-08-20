dd0b43713f8189ce441415cac8b7c5a0216a950b
ungheung <t01047377477@gmail.com>
2024-08-20 17:44:03 +0900

    feat: Looger 로그 추가, config를 활용한 포트 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.DATABASE_HOST;
+  const serverConfig: any = config.get('server');
+  const port = serverConfig.port;
   await app.listen(port);
+
+  Logger.log(`>>>>> 애플리케이션 시작. 포트 : ${port}`);
 }
 bootstrap();
