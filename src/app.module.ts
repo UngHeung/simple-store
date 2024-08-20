ecfa2338bea2daaac8aebf44fbd198f6ccac6f2b
ungheung <t01047377477@gmail.com>
2024-08-20 18:03:08 +0900

    feat : typeorm config 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development.local',
     }),
+    TypeOrmModule.forRoot(typeORMConfig),
   ],
 })
 export class AppModule {}
