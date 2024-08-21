a15c6d69315502a24e9fc89829a2cd502406dd5d
ungheung <t01047377477@gmail.com>
2024-08-21 22:21:49 +0900

    update: entity, auth 연결

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entity/product.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity]), AuthModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
