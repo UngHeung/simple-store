538573ba2e1075c1475ba4cf5cce34d9dd8f8609
ungheung <t01047377477@gmail.com>
2024-08-21 13:33:02 +0900

    feat : JWT 의존성 설치, import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import * as config from 'config';
+
+const jwtConfig: any = config.get('jwt');
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN || jwtConfig.expiresIn,
+      },
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
