6c62b66502bdabaf8ee0610f41bcb05ff84accb5
ungheung <t01047377477@gmail.com>
2024-08-21 22:24:07 +0900

    feat : 제조사 추가 코드

--- src/manufacturer/manufacturer.service.ts
+++ src/manufacturer/manufacturer.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ManufacturerEntity } from './entity/manufacturer.entity';
+import { Repository } from 'typeorm';
+import { AddManufacturerDto } from './dto/add-manufacturer.dto';
 
 @Injectable()
-export class ManufacturerService {}
+export class ManufacturerService {
+  @InjectRepository(ManufacturerEntity)
+  private manufacturerRepository: Repository<ManufacturerEntity>;
+
+  async addManufacturer(addManufacturerDto: AddManufacturerDto): Promise<void> {
+    const { manufacturerName } = addManufacturerDto;
+    const manufacturer = this.manufacturerRepository.create({
+      manufacturerName,
+    });
+
+    try {
+      await this.manufacturerRepository.save(manufacturer);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
