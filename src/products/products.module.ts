8b2e29c790daaecf05e99ab964050679d0457105
ungheung <t01047377477@gmail.com>
2024-08-22 23:11:06 +0900

    feat : multer 설정 파일

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,6 +6,8 @@ import { ProductEntity } from './entity/product.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { PriceByStorageEntity } from './entity/price-by-storage.entity';
 import { ManufacturerEntity } from 'src/manufacturer/entity/manufacturer.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfigService } from 'src/configs/multer.config';
 
 @Module({
   imports: [
@@ -15,6 +17,9 @@ import { ManufacturerEntity } from 'src/manufacturer/entity/manufacturer.entity'
       ManufacturerEntity,
     ]),
     AuthModule,
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
