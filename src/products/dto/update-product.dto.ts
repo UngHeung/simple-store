76cb44ee5abe35cfa535cbcb6205998f99feae0a
ungheung <t01047377477@gmail.com>
2024-08-22 13:42:20 +0900

    feat : 상품업데이트 dto

---
+++ src/products/dto/update-product.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class UpdateProductDto {
+  @IsNumber()
+  @IsNotEmpty()
+  productId: number;
+  @IsString()
+  modelName: string;
+  @IsString()
+  petName: string;
+  @IsString()
+  colors: string;
+}
