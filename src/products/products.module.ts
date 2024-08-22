89bb86ce3f396bc0f0899e9e2f5ad7b07f1eda89
ungheung <t01047377477@gmail.com>
2024-08-22 10:34:15 +0900

    feat : price by storage module 추가

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,9 +4,13 @@ import { ProductsService } from './products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './entity/product.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { PriceByStorageEntity } from './entity/price-by-storage.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([ProductEntity, PriceByStorageEntity]),
+    AuthModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
